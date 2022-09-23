65e926c9b2188f37ae589dad4b1300d3a87a6f51
LorenzoLatt <lo_costa@outlook.com>
2022-09-22 22:17:30 -0300

    made some changes to the views

--- src/main/java/com/example/demo/models/Contato.java
+++ src/main/java/com/example/demo/models/Contato.java
@@ -1,6 +1,5 @@
 package com.example.demo.models;
 
-
 import javax.persistence.*;
 
 @Entity
@@ -15,9 +14,18 @@ public class Contato {
     private String nome;
     @Column(length = 100, nullable = false)
     private String email;
+
     @Column(length = 100, nullable = false)
     private String numero;
 
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
 
     public int getId() {
         return id;
@@ -41,13 +49,5 @@ public class Contato {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
     }
 }
